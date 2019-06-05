15d2c21b4a32e7015af315e9cb62896780736ce8
Jake <jakejhdz@gmail.com>
2019-06-05 11:01:46 -0700

    fixed thing that makes stuff bigger

--- javascript/events/myscript.js
+++ javascript/events/myscript.js
@@ -3,14 +3,15 @@ var button = document.querySelector("button");
 button.addEventListener("click", function(){
   popup.classList.toggle("invisible");
   popup.classList.toggle("visible");
-  alert("You are seen");
 })
 
 var lis = document.querySelectorAll("li");
-
 for (var i = 0; i < lis.length; i++) {
   lis[i].addEventListener("click", function(){
-    
-    this.style.fontSize = String(size) + "px";
+    if (typeof this.num == "undefined") {
+      this.num = 15;
+    }
+      this.num += 1;
+    this.style.fontSize = String(this.num) + "px";
   })
 }
