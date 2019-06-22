990535f77238d4aae3dea9680954f03dc8f81111
Jake <jakejhdz@gmail.com>
2019-06-21 21:29:01 -0700

    finished todo list

--- todo-list/assets/js/script.js
+++ todo-list/assets/js/script.js
@@ -1,18 +1,27 @@
 
 
-$("input").keypress(function(event){
+$("input[type='text']").keypress(function(event){
     // if event has keypress enter (13)
     if (event.which === 13) {
-        $("ul").append("<li><span>X</span> " + $("input").val() + "</li>");
+        $("ul").append("<li><span><i class='fas fa-trash'></i></span> " + $("input").val() + "</li>");
         $("input").val("");
     }
-})
+});
 
-$("ul").on("click", "span", function() {
+$("ul").on("click", "span", function(event) {
     // remove parent element (li in this case)
-    $(this).parent().remove();
-})
+    event.stopPropagation();
+    $(this).parent().fadeOut(500, function() {
+        // $(this) now refers to the PARENT element since we called the parent function. 
+        $(this).remove();
+    });
+    
+});
 
 $("ul").on("click", "li", function() {
     $(this).toggleClass("selected");
-})
+});
+
+$(".fa-plus").click(function() {
+    $("input[type='text'").fadeToggle();
+});
