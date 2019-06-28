07449538f517b7f108c599ddc7e4c0ff8dd3da6f
Jake <jakejhdz@gmail.com>
2019-06-28 14:18:30 -0700

    more express stuff

--- backend/IntroToExpress/FirstExpressApp/app.js
+++ backend/IntroToExpress/FirstExpressApp/app.js
@@ -10,11 +10,25 @@ app.get("/", function(req, res){
 app.get("/bye", function(req, res){
     res.send("goodbye");
 });
-// "/dog" => "MEOW!"
+// "/dog" => "Woof!"
 app.get("/dog", function(req, res) {
-    res.send("Woof");
+    res.send("Meow");
     console.log("someone made a request to dog!");
 });
+
+app.get("/r/:subredditName", function(req, res){
+    var subreddit = req.params.subredditName;
+    res.send("Welcome to " + subreddit);
+});
+
+app.get("/r/:subredditName/comments/:id/:title/", function(req, res) {
+    res.send("Welcome to the comments page!");
+})
+app.get("*", function(req, res){
+    res.send("404 Not found");
+});
+
+
 app.listen(8000, function() {
     console.log("Server has started!");
 });
