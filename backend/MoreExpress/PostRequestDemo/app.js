928b16bb7834d380c6ac317f0a6d208b549d34cc
Jake <jakejhdz@gmail.com>
2019-06-29 22:09:12 -0700

    some ejs

---
+++ backend/MoreExpress/PostRequestDemo/app.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended:true}));
+// set express to look for ejs files
+app.set("view engine", "ejs");
+
+// default route
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/friends", function(req,res){
+    var friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
+    res.render("friends", {friends: friends});
+});
+
+app.post("/addfriend", function(req, res){
+    console.log(req.body);
+    res.send("You have attempted to add a friend");
+})
+// server listen
+app.listen(8000, function(){
+    console.log("Server started.")
+});
